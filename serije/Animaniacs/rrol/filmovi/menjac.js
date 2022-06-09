7b1723025369a2c0e0de891111701f94ca91a7f9
ValoaEntertainment <105073401+ValoaEntertainment@users.noreply.github.com>
2022-06-10 01:13:05 +0200

    Add files via upload

---
+++ serije/Animaniacs/rrol/filmovi/menjac.js
@@ -0,0 +1,82 @@
+function menjacserijeE1() {
+  document.getElementById("serija").src = "../All Star.mp4";
+}
+function menjacserijeE2() {
+  document.getElementById("serija").src = "../Welcome to the Internet.mp4";
+}
+function menjacserijeE3() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/949dccb6-3116-4201-9152-6543b2803645?autoplay=false&showinfo=false";
+}
+function menjacserijeE4() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/18a59610-7cfc-40d5-9588-a2c478e23e3d?autoplay=false&showinfo=false";
+}
+function menjacserijeE5() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/a99ff992-3699-4ce4-9fbc-6c7b5dfc019d?autoplay=false&showinfo=false";
+}
+function menjacserijeE6() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/876ca227-cf89-4354-9aea-64252cd21bb4?autoplay=false&showinfo=false";
+}
+function menjacserijeE7() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/9a89521b-a9a8-43ba-ac0e-9d670fd38b6a?autoplay=false&showinfo=false";
+}
+function menjacserijeE8() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/20c5e3f7-ed10-4406-be39-33b0e1694be7?autoplay=false&showinfo=false";
+}
+function menjacserijeE9() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/9a86f1c3-0a58-49f5-a914-a8b067bc6068?autoplay=false&showinfo=false";
+}
+function menjacserijeE10() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/c8e82450-23b4-40e9-af7e-0db2c14cf454?autoplay=false&showinfo=false";
+}
+function menjacserijeE11() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/a781c370-896f-49c0-a79e-49c63a3fbaf3?autoplay=false&showinfo=false";
+}
+function menjacserijeE12() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/33815ce4-32d9-45a2-a70c-5cfbbf293953?autoplay=false&showinfo=false";
+}
+function menjacserijeE13() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/a79ac806-7292-427d-851c-501432999b42?autoplay=false&showinfo=false";
+}
+
+
+
+
+function menjacserijeS2E1() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/c9383874-34a7-4be7-a294-ecaddaed8933?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E2() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/41c945e4-8c71-43d0-b7d7-f0b59a326c67?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E3() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/9de75c20-499d-45a5-a0b1-3c856f9ab690?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E4() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/381133b9-9487-444c-9dca-11f3f8a22b60?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E5() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/48d857b7-660c-4a57-a738-a6a4211e45fc?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E6() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/d5c08fc8-6e32-4ccb-97dc-ce13c88acacb?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E7() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/76cdf735-872a-4afc-9249-5e29a96217a0?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E8() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/c42cc9d6-9d9e-45d2-8e8c-841c52bcbe0d?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E9() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/ccaef904-8169-4e5d-8944-474b5b2b402a?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E10() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/eb38eb3f-de3a-4e7d-92ab-b3821f9bf806?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E11() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/0357c41d-6a84-47bd-894e-225242f3d750?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E12() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/2be0b7d8-e6ef-42a8-b697-28b07a4b7a59?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E13() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/ed0fbc46-9c2b-4fba-a393-37feebeed03f?autoplay=false&showinfo=false";
+}
---
+++ serije/GravityFalls/filmovi/menjac.js
@@ -0,0 +1,124 @@
+
+function menjacserijeE2() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/e4e0688e-3625-455c-b3a4-48ec564f0876?autoplay=false&showinfo=false";
+}
+function menjacserijeE3() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/8a5a3a96-502b-4af6-9885-3d340efc0605?autoplay=false&showinfo=false";
+}
+function menjacserijeE4() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/3b4b648a-cf1b-4954-8a83-b1150e8453d2?autoplay=false&showinfo=false";
+}
+function menjacserijeE5() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/aca9668f-447e-4afb-8087-2e8fb588a089?autoplay=false&showinfo=false";
+}
+function menjacserijeE6() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/6ae985ba-4a95-48bd-9824-d74ec5c8697d?autoplay=false&showinfo=false";
+}
+function menjacserijeE7() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/45cd237e-d45d-414f-949f-374d8834e766?autoplay=false&showinfo=false";
+}
+function menjacserijeE8() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/ae5858f7-3dcd-4e75-b362-fee0d50375ef?autoplay=false&showinfo=false";
+}
+function menjacserijeE9() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/afaab341-8c8a-4f6d-9600-f54ef0b5231f?autoplay=false&showinfo=false";
+}
+function menjacserijeE10() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/789f90c8-5e2f-4002-8bb4-e4da804b8894?autoplay=false&showinfo=false";
+}
+function menjacserijeE11() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/92343358-6406-4eb2-81ce-d56921fb31a5?autoplay=false&showinfo=false";
+}
+function menjacserijeE12() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/ebe68069-1f9b-4e29-abe1-77026503c60a?autoplay=false&showinfo=false";
+}
+function menjacserijeE13() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/907e16de-ba9c-4ca2-96b6-e9f3394a0599?autoplay=false&showinfo=false";
+}
+function menjacserijeE14() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/17ba1c0c-8fe2-4e1b-b281-72fa1fe04ba1?autoplay=false&showinfo=false";
+}
+function menjacserijeE15() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/2ee76ace-2195-4b2c-9d04-23f82947eb9b?autoplay=false&showinfo=false";
+}
+function menjacserijeE16() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/b7f9cd46-58ee-4152-a532-025faee5af90?autoplay=false&showinfo=false";
+}
+function menjacserijeE17() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/7ab1456d-8671-45f5-aebd-82a383c38afc?autoplay=false&showinfo=false";
+}
+function menjacserijeE18() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/cc1aa1c8-1a64-46d7-b975-c72abc90166c?autoplay=false&showinfo=false";
+}
+function menjacserijeE19() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/548f9652-666c-4230-8ee6-cfe59a0fd0cb?autoplay=false&showinfo=false";
+}
+function menjacserijeE20() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/d281789a-5fa3-4821-9d53-f643ce8bd916?autoplay=false&showinfo=false";
+}
+
+
+
+
+
+
+function menjacserijeS2E1() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/075343d1-6752-4837-af14-61296a6baaa5?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E2() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/b40b7999-0160-43bb-860d-f2a5e9f18116?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E3() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/92d67db1-da96-4b72-9042-3d3f700c8f9d?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E4() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/a72515e0-6c48-4b8b-b9f6-5df54dbc1388?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E5() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/3bf25307-3807-4338-99f9-b874c8201abb?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E6() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/9f23d3af-8bca-4bcb-9120-ef30c6a36627?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E7() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/24529433-0953-403d-abe8-a58dd97653c0?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E8() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/b8d6f349-ae97-46da-a1b2-7be65ad40df1?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E9() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/6288d940-9db6-4e46-ab10-e26174cfd6e8?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E10() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/a810510c-6346-4ede-877d-2c2649b712f1?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E11() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/d85a362c-3b29-42ec-bcb2-1a0d382fbea1?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E12() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/f70a3156-7b1a-487f-b361-be00ed29b020?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E13() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/4ebfd815-7dcf-4271-831a-442456a01c08?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E14() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/3a10f5f5-15e1-4ebf-8408-6dcf54c68d68?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E15() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/23778e79-d5cf-47d0-92ea-b6a3539997d4?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E16() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/b77101d9-234e-4708-bda3-f016f204d198?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E17() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/1fcdb394-0368-4453-a39f-d40da827866f?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E18() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/0d198555-c438-4c96-a08e-07be8c358ff5?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E19() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/3e2209a7-0dee-4e83-ac17-7a8bdae9b8c6?autoplay=false&showinfo=false";
+}
+function menjacserijeS2E20() {
+  document.getElementById("serija").src = "https://web.microsoftstream.com/embed/video/b9abf0e4-f054-4860-b161-9c5d2b1274bd?autoplay=false&showinfo=false";
+}
